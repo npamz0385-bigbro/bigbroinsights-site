718f91d9d49065223e1be9e4161f48f0a99dda94
npamz0385-bigbro <npamz0385@gmail.com>
2025-12-07 19:42:57 +0530

    Delete examples directory

--- examples/framework-alpine/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-import alpine from '@astrojs/alpinejs';
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [alpine()],
-});
--- examples/with-markdoc/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-import markdoc from '@astrojs/markdoc';
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [markdoc()],
-});
